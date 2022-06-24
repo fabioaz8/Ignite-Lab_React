4f3d3e354be7045e8052f6d00e894aa66b254b75
fabioaz8 <fabioaz8@gmail.com>
2022-06-24 20:37:47 -0300

    Final da Aula 04 24/06/2022

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4q0wos72tjg01xl4e6kg9bi/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
-})
+});
